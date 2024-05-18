b5336bce2cb29e20bfe3c908047bd1edc5666622
홍정민 <dream7739@naver.com>
2024-05-18 19:11:44 +0900

    Tuple로 구현, UIButton tag 이용 방식

---
+++ Emotion/EmotionViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  EmotionViewController2.swift
+//  Emotion
+//
+//  Created by 홍정민 on 5/18/24.
+//
+
+import UIKit
+
+class EmotionViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
