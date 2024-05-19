eb151fffe0bc6c06638126b0e06ef45a0e7a2d49
홍정민 <dream7739@naver.com>
2024-05-19 16:41:51 +0900

    신조어 사전 해시태그 클릭 시 랜덤으로 변경되는 기능 추가

--- Emotion/WordViewController.swift
+++ Emotion/WordViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class WordViewController: UIViewController {
-
+    
     @IBOutlet var textBackgroundView: UIView!
     @IBOutlet var searchTextField: UITextField!
     @IBOutlet var searchButton: UIButton!
@@ -31,6 +31,7 @@ class WordViewController: UIViewController {
         let keyword = sender.currentTitle!
         searchWord(keyword: keyword)
         searchTextField.text = keyword
+        getRandomKeyword()
     }
     
     //검색 버튼 클릭 시
@@ -40,7 +41,17 @@ class WordViewController: UIViewController {
         getRandomKeyword()
     }
     
-    private var wordDictionary: [String: String] = ["다꾸": "다이어리 꾸미기", "오하운":"오늘 하루 운동", "폼 미쳤다": "역량이 뛰어나다는 의미. 때로는 비꼬는 의미로 사용하기도 함.", "제당슈만": "제가 당신을 슈퍼스타로 만들어 드릴게요.", "무물보": "무엇이든 물어보세요", "갑통알": "갑자기 통장을 보니 알바를 해야겠다.", "알잘딱깔센": "알아서 잘 딱 깔끔하고 센스 있게", "아만추": "아무나 만남 추구", "꾸안꾸": "꾸민 듯 안꾸민듯", "삼귀다": "사귀기 전 사이", "혼코노": "혼자 코인노래방 가기"]
+    private var wordDictionary: [String: String] = ["다꾸": "다이어리 꾸미기",
+                                                    "오하운":"오늘 하루 운동",
+                                                    "폼 미쳤다": "역량이 뛰어나다는 의미. 때로는 비꼬는 의미로 사용하기도 함.",
+                                                    "제당슈만": "제가 당신을 슈퍼스타로 만들어 드릴게요.",
+                                                    "무물보": "무엇이든 물어보세요",
+                                                    "갑통알": "갑자기 통장을 보니 알바를 해야겠다.",
+                                                    "알잘딱깔센": "알아서 잘 딱 깔끔하고 센스 있게",
+                                                    "아만추": "아무나 만남 추구",
+                                                    "꾸안꾸": "꾸민 듯 안꾸민듯",
+                                                    "삼귀다": "사귀기 전 사이",
+                                                    "혼코노": "혼자 코인노래방 가기"]
     
     private var randomKeyword:[String] = []
     
@@ -53,6 +64,7 @@ class WordViewController: UIViewController {
         searchTextField.borderStyle = .none
         searchTextField.tintColor = .black
         searchTextField.placeholder = "신조어를 입력하세요"
+        
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
         searchTextField.leftView = paddingView
         searchTextField.clearButtonMode = .always
@@ -63,11 +75,10 @@ class WordViewController: UIViewController {
         searchButton.setTitle("", for: .normal)
         searchButton.tintColor = .white
         
-        
-        applyButtonStyle(hashTagButton1)
-        applyButtonStyle(hashTagButton2)
-        applyButtonStyle(hashTagButton3)
-        applyButtonStyle(hashTagButton4)
+        designButton(hashTagButton1)
+        designButton(hashTagButton2)
+        designButton(hashTagButton3)
+        designButton(hashTagButton4)
         
         mentboxImageView.contentMode = .scaleAspectFill
         mentboxImageView.image = UIImage(named: "background")
@@ -79,13 +90,13 @@ class WordViewController: UIViewController {
         
         //랜덤 키워드 설정
         getRandomKeyword()
-
+        
     }
     
     //해시태그 버튼을 랜덤 키워드로 변경
     private func getRandomKeyword(){
         randomKeyword = []
-
+        
         while randomKeyword.count < 5 {
             let keyword = wordDictionary.randomElement()!.key
             if !randomKeyword.contains(keyword) && keyword.count < 4{
@@ -110,11 +121,11 @@ class WordViewController: UIViewController {
     }
     
     //버튼 스타일 적용
-    private func applyButtonStyle(_ btn: UIButton){
-        btn.layer.borderColor = UIColor.black.cgColor
-        btn.layer.cornerRadius = 15
-        btn.layer.borderWidth = 2
-        btn.tintColor = .black
+    fileprivate func designButton(_ sender: UIButton){
+        sender.layer.borderColor = UIColor.black.cgColor
+        sender.layer.cornerRadius = 15
+        sender.layer.borderWidth = 2
+        sender.tintColor = .black
     }
     
     
@@ -123,6 +134,6 @@ class WordViewController: UIViewController {
         view.endEditing(true)
     }
     
-
-  
+    
+    
 }
